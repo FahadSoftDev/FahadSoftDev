b86917eaf2da8cfcbf3bb8da84ab75a2ba23eb30
Fahad Hossain <152505290+FahadSoftDev@users.noreply.github.com>
2025-01-03 13:05:59 +0600

    Update Index.js
    Inheritance

--- Index.js
+++ Index.js
@@ -3,3 +3,24 @@ console.log("hellos")
 const apple = 24
 const orange = 25
 console.log(apple + orange)
+
+class automobile {
+    constructor(name, cc) {
+        this.name = name;
+        this.cc = cc;
+    }
+    engine() {
+        console.log(`${this.name} 
+      has ${this.cc} engine`);
+    }
+}
+
+class car extends automobile {
+    engine() {
+        console.log(this.name,
+            &quot;has &quot;, this.cc, &quot;cc engine&quot;);
+    }
+}
+
+let carz = new car('Rex', &quot;1149&quot;);
+carz.engine();
